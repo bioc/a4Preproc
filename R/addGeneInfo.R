dbde418cd496a6d57e34a3f43efcf3156ef4efd5
Martin Morgan <mtmorgan@fhcrc.org>
2012-03-12 00:44:52 +0000

    use mget generic from BiocGenerics
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/a4Preproc@63751 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/addGeneInfo.R
+++ R/addGeneInfo.R
@@ -13,19 +13,19 @@ addGeneInfo <- function(eset, annotationLibrary = NULL){
 	fNames <- featureNames(eset)
 	
 	### ENTREZID
-	pData(featureData(eset))[,1] <- unlist(AnnotationDbi:::mget(fNames, eval(parse(text=paste(annotationLibrary, "ENTREZID", sep="")))))[fNames]   
+	pData(featureData(eset))[,1] <- unlist(mget(fNames, eval(parse(text=paste(annotationLibrary, "ENTREZID", sep="")))))[fNames]   
 	colnames(pData(featureData(eset)))[1] <- "ENTREZID"
 	fvarMetadata(eset)[1,1] <- "Entrez ID as retrieved from annotation package"
 	### ENSEMBL ID
-	pData(featureData(eset))[,2] <- unlist(AnnotationDbi:::mget(fNames,eval(parse(text=paste(annotationLibrary, "ENSEMBL", sep="")))))[fNames]
+	pData(featureData(eset))[,2] <- unlist(mget(fNames,eval(parse(text=paste(annotationLibrary, "ENSEMBL", sep="")))))[fNames]
 	colnames(pData(featureData(eset)))[2] <- "ENSEMBLID"
 	fvarMetadata(eset)[2,1] <- "Ensembl ID as retrieved from annotation package"
 	### Gene Symbol
-	pData(featureData(eset))[,3] <- unlist(AnnotationDbi:::mget(fNames,eval(parse(text=paste(annotationLibrary, "SYMBOL", sep="")))))[fNames]
+	pData(featureData(eset))[,3] <- unlist(mget(fNames,eval(parse(text=paste(annotationLibrary, "SYMBOL", sep="")))))[fNames]
 	colnames(pData(featureData(eset)))[3] <- "SYMBOL"
 	fvarMetadata(eset)[3,1] <- "Gene symbol as retrieved from annotation package"
 	### Description
-	pData(featureData(eset))[,4] <- unlist(AnnotationDbi:::mget(fNames,eval(parse(text=paste(annotationLibrary, "GENENAME", sep="")))))[fNames]
+	pData(featureData(eset))[,4] <- unlist(mget(fNames,eval(parse(text=paste(annotationLibrary, "GENENAME", sep="")))))[fNames]
 	colnames(pData(featureData(eset)))[4] <- "GENENAME"
 	fvarMetadata(eset)[4,1] <- "Description as retrieved from annotation package"
 	return(eset)
